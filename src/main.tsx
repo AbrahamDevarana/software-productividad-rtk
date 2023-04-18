bb8bf0a6c938a19814dddbc7870d7aa02c20a174
Abraham Alvarado <abrahamag93@gmail.com>
2023-04-17 18:47:21 -0600

    Ajuste PLuxon

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,8 @@ import ReactDOM from 'react-dom/client'
 import { ConfigProvider } from "antd";
 import es_ES from 'antd/es/locale/es_ES';
 import App from './app';
-import { Settings } from "luxon";
 import 'dayjs/locale/es'
 import { BrowserRouter } from 'react-router-dom';
-Settings.defaultLocale = "es";
-
-
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     // <React.StrictMode>
