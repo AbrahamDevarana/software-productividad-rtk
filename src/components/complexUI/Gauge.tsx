985e163d65005ae6d6aeeb8b2941a39a16961fd6
Abraham Alvarado <abrahamag93@gmail.com>
2023-07-25 17:28:22 -0600

    Ajustes

--- src/components/complexUI/Gauge.tsx
+++ src/components/complexUI/Gauge.tsx
@@ -6,10 +6,12 @@ const GaugeChart = () => {
     return ( 
     <>
         <div className='w-[350px]'>
-            <Gauge id="gauge" analogDisplayType="needle" digitalDisplay startAngle={-30} endAngle={210} min={0} max={100} value={0} 
-                customInterval customTicks={[0, 20, 40, 60, 80, 100]} style={{
+            <Gauge precisionDigits={0}  customTicks={[0, 20, 40, 60, 80, 100]} style={{
                     width: '100%',
-                }}
+                }}  value={80}
+                 
+               
+
                 />
         </div>
     </> );
